66d8dad3f58394e3497b0f04fd02ccf35b0e5935
Weronka Golonka <54840864+weronkagolonka@users.noreply.github.com>
2020-02-06 12:20:28 +0100

    small changes

--- does_it_fit/lib/main.dart
+++ does_it_fit/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Does it Fit?',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -18,9 +19,11 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+
+        //it has to be materialColor type
+        primarySwatch: Colors.blueGrey,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Does it fit?'),
     );
   }
 }
